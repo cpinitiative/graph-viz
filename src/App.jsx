a54d7c39340e778a4a64a18e6d59ebce4de06422
emhgit <eharper0815@gmail.com>
2026-05-31 20:36:02 -0500

    Add navbar component and update styling for edge labels

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import GraphStudioVisualizer from "./components/visualizers/Graphs/GraphStudioVisualizer";
 import { ThemeProvider } from "./context/ThemeContext";
-import ThemeToggle from "./components/ThemeToggle";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <ThemeProvider>
       <div className="h-screen w-screen overflow-hidden bg-surface font-inter text-on-surface dark:bg-dark-surface dark:text-dark-on-surface">
-        <div className="absolute top-4 right-4 z-50">
-          <ThemeToggle />
-        </div>
+        <NavBar />
         <GraphStudioVisualizer snapshot={{}} />
       </div>
     </ThemeProvider>
